336d7c39165c162aa4b209c1df02b9b529231756
Joaco Torres <joaquintorresfripp@hotmail.com>
2025-02-05 13:33:25 -0300

    Add create user return when errors

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,22 +1,38 @@
-import User from "../config/models/User"
 import { Request, Response } from "express";
+import slug from 'slug'
+import User from "../config/models/User"
 import { hashPassword } from "../utils/auth";
 
 
 export const createAccount = async (req: Request, res: Response): Promise<void> => {
     try {
         const { email, password } = req.body        
+        
+        // Check Email Exist
         const userExist = await User.findOne({ email });
-
         if (userExist) {
             res.status(409).json({ error: 'El usuario ya está registrado' });
+            return
         }
 
+        // Check Handle Exist
+        const handle = slug(req.body.handle, '');
+        const handleExist = await User.findOne({ handle });
+        
+        if (handleExist) {
+            res.status(409).json({ error: 'Nombre de usuario no disponible' });
+            return
+        }
+        
+        // Change user data
         const user = new User(req.body);
+        user.handle = handle
         user.password = await hashPassword(password)
+
         await user.save();
 
         res.status(201).send('Registro creado correctamente');
+
     } catch (error) {
         res.status(500).json({ error: 'Error interno del servidor' });
     }
