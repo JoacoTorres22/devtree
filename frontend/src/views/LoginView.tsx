e42728a96516f1ccee9431aedf0ec81a912ad771
Joaco Casa <joaquintorres@studio4d.com>
2025-02-19 17:43:59 -0300

    Project end

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import ErrorMessage from "../components/ErrorMessage";
@@ -7,7 +7,7 @@ import api from "../config/axios";
 import { isAxiosError } from "axios";
 
 export default function LoginView() {
-
+    const navigate = useNavigate()
     const initialValues : LoginForm  = {
         email: '',
         password: ''
@@ -18,6 +18,7 @@ export default function LoginView() {
         try {
             const { data } = await api.post(`/auth/login`, formData)
             localStorage.setItem('AUTH_TOKEN', data)
+            navigate('/admin')
         } catch (error) {
             if (isAxiosError(error))
                 toast.error(error.response?.data.error)
